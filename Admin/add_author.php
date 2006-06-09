b853aaed0abf231437ceace52c81c0d80dd2f255
aicmltec <aicmltec>
2006-06-09 23:18:00 +0000

    converting to use pdHtmlPage

--- Admin/add_author.php
+++ Admin/add_author.php
@@ -1,6 +1,6 @@
 <?php ;
 
-// $Id: add_author.php,v 1.10 2006/06/07 02:19:36 aicmltec Exp $
+// $Id: add_author.php,v 1.11 2006/06/09 23:18:00 aicmltec Exp $
 
 /**
  * \file
@@ -24,240 +24,239 @@
 
 ini_set("include_path", ini_get("include_path") . ":..");
 
-require_once 'includes/functions.php';
-require_once 'includes/check_login.php';
-require_once 'includes/pageConfig.php';
+require_once 'includes/pdHtmlPage.php';
 require_once 'includes/pdAuthInterests.php';
 
-require_once 'HTML/QuickForm.php';
-require_once 'HTML/Table.php';
+/**
+ * Renders the whole page.
+ */
+class add_author extends pdHtmlPage {
+    function add_author() {
+        global $logged_in;
 
-htmlHeader('add_author', 'Add Author');
+        parent::pdHtmlPage('add_author');
 
-if (!$logged_in) {
-    echo '<body>';
-    pageHeader();
-    navMenu('add_publication');
-    echo "<div id='content'>\n";
-    loginErrorMessage();
-}
-
-?>
-
-<script language="JavaScript" type="text/JavaScript">
-    var addAuthorPageHelp=
-    "This window is used to add an author to the database. In order to add "
-    + "an author you need to input the author's first name, last name, "
-    + "email address and organization. You must also select interet(s) that "
-    + "the author has. To do this you can select interest(s) allready in the "
-    + "database by selecting them from the listbox. You can select multiple "
-    + "interests by control-clicking on them. If you do not see the "
-    + "appropriate interest(s) you can add interest(s) using the Add Interest "
-    + "link.<br/><br/>"
-    + "Clicking the Add Interest link will bring up additional fields "
-    + "everytime you click it. You can then type in the name of the interest "
-    + "into the new field provided.";
-
-var authTitleHelp=
-    "The title of an author. Will take the form of one of: {Prof, PostDoc, "
-    + "PhD student, MSc student, Colleague, etc...}.";
-
-    function verify() {
-    if (document.forms["authorForm"].elements["firstname"].value == "") {
-        alert("Please enter a complete name for the new author.");
-        return false;
-    }
-    if (document.forms["authorForm"].elements["lastname"].value == "") {
-        alert("Please enter a complete name for the new author.");
-        return false;
-    }
-    if ((document.forms["authorForm"].elements["firstname"].value).search(",")
-        !=-1){
-        alert("Please do not use commas in author's first name");
-        return false;
-    }
-    if ((document.forms["authorForm"].elements["lastname"].value).search(",")
-        !=-1){
-        alert("Please do not use commas in author's last name");
-        return false;
-    }
-    return true;
-}
-
-function dataKeep(num) {
-    var temp_qs = "";
-    var info_counter = 0;
-
-    for (i = 0; i < document.forms["authorForm"].elements.length; i++) {
-        if ((document.forms["authorForm"].elements[i].value != "")
-            && (document.forms["authorForm"].elements[i].value != null)) {
-            if (info_counter > 0) {
-                temp_qs = temp_qs + "&";
-            }
-
-            if (document.forms["authorForm"].elements[i].name == "interests[]") {
-                interest_array
-                    = document.forms["authorForm"].elements['interests[]'];
-                var interest_list = "";
-                var interest_count = 0;
-
-                for (j = 0; j < interest_array.length; j++) {
-                    if (interest_array[j].selected == 1) {
-                        if (interest_count > 0) {
-                            interest_list = interest_list + "&";
-                        }
-                        interest_list = interest_list
-                            + "interests[" + j + "]="
-                            + interest_array[j].value;
-                        interest_count++;
-                    }
-                }
-
-                temp_qs = temp_qs + interest_list;
-            }
-            else {
-                temp_qs = temp_qs
-                    + document.forms["authorForm"].elements[i].name + "="
-                    + document.forms["authorForm"].elements[i].value;
-            }
-
-            info_counter++;
+        if (!$logged_in) {
+            $this->loginError = true;
+            return;
         }
+
+        // Connecting, selecting database
+        $db =& dbCreate();
+
+        if (isset($_GET['newInterests'])) {
+            $newInterests =  $_GET['newInterests'] + 1;
+        }
+        else {
+            $newInterests = 0;
+        }
+        $this->contentPre
+            .= '<h3>' . helpTooltip('Add Author', 'addAuthorPageHelp') . '</h3>';
+
+        $formAttr = array();
+        if($_GET['popup'] != "false")
+            $formAttr = array('onsubmit' => "setTimeout('self.close()',0);");
+
+        $form = new HTML_QuickForm('authorForm', 'post',
+                                   "./add_publication.php?"
+                                   . $_SERVER['QUERY_STRING'],
+                                   "add_publication.php",
+                                   $formAttr);
+
+        $form->addElement('text', 'firstname', null,
+                          array('size' => 50, 'maxlength' => 250));
+        $form->addElement('text', 'lastname', null,
+                          array('size' => 50, 'maxlength' => 250));
+        $form->addElement('text', 'auth_title', null,
+                          array('size' => 50, 'maxlength' => 250));
+        $form->addElement('text', 'email', null,
+                          array('size' => 50, 'maxlength' => 250));
+        $form->addElement('text', 'organization', null,
+                          array('size' => 50, 'maxlength' => 250));
+        $form->addElement('text', 'webpage', null,
+                          array('size' => 50, 'maxlength' => 250));
+
+        $interests = new pdAuthInterests();
+        $interests->dbLoad($db);
+        assert('is_array($interests->list)');
+        foreach($interests->list as $intr) {
+            $options[$intr->interest_id] = $intr->interest;
+        }
+
+        $form->addElement('select', 'interests', null, $options,
+                          array('multiple' => 'multiple', 'size' => 4));
+
+        $form->addElement('submit', 'Submit', 'Add Author',
+                          array('onClick' => 'return verify();'));
+        $form->addElement('reset', 'Reset', 'Reset',
+                          array('class' => 'text',
+                                'onClick' => 'resetAll();'));
+
+        if ($_GET['popup'])
+            $form->addElement('reset', 'Cancel', 'Cancel',
+                              array('class' => 'text',
+                                    'onClick' => 'closeWindow();'));
+        else
+            $form->addElement('reset', 'Cancel', 'Cancel',
+                              array('class' => 'text',
+                                    'onClick' => 'history.back();'));
+
+        $form->addElement('hidden', 'newAuthorSubmitted', 'true');
+        $form->addElement('hidden', 'numInterests', $counter + 1);
+        $form->addElement('hidden', 'fromauthorspage', 'true');
+
+        for ($i = 0; $i < $newInterests; $i++) {
+            $form->addElement('text', 'newInterest' . $i, null,
+                              array('size' => 50, 'maxlength' => 250));
+        }
+
+        $form->setDefaults($_GET);
+
+        $renderer =& new HTML_QuickForm_Renderer_QuickHtml();
+        $form->accept($renderer);
+
+
+        $tableAttrs = array('width' => '100%',
+                            'border' => '0',
+                            'cellpadding' => '6',
+                            'cellspacing' => '0');
+        $table = new HTML_Table($tableAttrs);
+        $table->setAutoGrow(true);
+
+        $table->addRow(array('First Name:',
+                             $renderer->elementToHtml('firstname')));
+        $table->addRow(array('Last Name:',
+                             $renderer->elementToHtml('lastname')));
+        $table->addRow(array(helpTooltip('Title', 'authTitleHelp') . ':',
+                             $renderer->elementToHtml('auth_title')));
+        $table->addRow(array('Email:', $renderer->elementToHtml('email')));
+        $table->addRow(array('Organization:',
+                             $renderer->elementToHtml('organization')));
+        $table->addRow(array('Webpage:', $renderer->elementToHtml('webpage')));
+
+        $ref = '<br/><div id="small"><a href="javascript:dataKeep('
+            . $newInterests .')">[Add Interest]</a></div>';
+
+        $table->addRow(array('Interests:' . $ref,
+                             $renderer->elementToHtml('interests')));
+
+        for ($i = 0; $i < $newInterests; $i++) {
+            $table->addRow(array('Interest Name:',
+                                 $renderer->elementToHtml('newInterest' . $i)));
+        }
+
+        $this->form =& $form;
+        $this->renderer =& $renderer;
+        $this->table =& $table;
+        $this->javascript();
+        $db->close();
     }
 
-    temp_qs = temp_qs.replace(" ", "%20");
-    temp_qs = temp_qs.replace("\"", "?");
-    location.replace("./add_author.php?<? echo $_SERVER['QUERY_STRING'] ?>&newInterests=" + num + "&" + temp_qs);
-}
-function closewindow() {window.close();}
+    function javascript() {
+        $this->js = <<<JS_END
 
-function gotoAuthors() {
-    location.replace("../list_author.php?type=view&admin=true&newauthor=true");
-}
+            <script language="JavaScript" type="text/JavaScript">
+            var addAuthorPageHelp=
+            "This window is used to add an author to the database. In order "
+            + "to add an author you need to input the author's first name, "
+            + "last name, email address and organization. You must also "
+            + "select interet(s) that the author has. To do this you can "
+            + "select interest(s) allready in the database by selecting "
+            + "them from the listbox. You can select multiple interests "
+            + "by control-clicking on them. If you do not see the "
+            + "appropriate interest(s) you can add interest(s) using "
+            + "the Add Interest link.<br/><br/>"
+            + "Clicking the Add Interest link will bring up additional fields "
+            + "everytime you click it. You can then type in the name of the "
+            + "interest into the new field provided.";
 
-function resetAll() {
-    location.replace("./add_author.php?<? echo $_SERVER['QUERY_STRING'] . "&newInterests=0" ?>");
-}
-</script>
+        var authTitleHelp=
+            "The title of an author. Will take the form of one of: "
+            + "{Prof, PostDoc, PhD student, MSc student, Colleague, etc...}.";
 
-<?php ;
+        function verify() {
+            var form = document.forms["authorForm"];
+            if (form.elements["firstname"].value == "") {
+                alert("Please enter a complete name for the new author.");
+                return false;
+            }
+            if (form.elements["lastname"].value == "") {
+                alert("Please enter a complete name for the new author.");
+                return false;
+            }
+            if ((form.elements["firstname"].value).search(",")
+                !=-1){
+                alert("Please do not use commas in author's first name");
+                return false;
+            }
+            if ((form.elements["lastname"].value).search(",")
+                !=-1){
+                alert("Please do not use commas in author's last name");
+                return false;
+            }
+            return true;
+        }
 
-echo "<body>\n";
+        function dataKeep(num) {
+            var temp_qs = "";
+            var info_counter = 0;
 
-if ($_GET['popup'] != 'true') {
-    pageHeader();
-    navMenu('add_author');
-    echo "<div id='content'>\n";
-}
+            var form = document.forms["authorForm"];
+            for (i = 0; i < form.elements.length; i++) {
+                if ((form.elements[i].value != "")
+                    && (form.elements[i].value != null)) {
+                    if (info_counter > 0) {
+                        temp_qs = temp_qs + "&";
+                    }
 
-/* Connecting, selecting database */
-$db =& dbCreate();
+                    if (form.elements[i].name == "interests[]") {
+                        interest_array = form.elements['interests[]'];
+                        var interest_list = "";
+                        var interest_count = 0;
 
-if (isset($_GET['newInterests'])) {
-    $newInterests =  $_GET['newInterests'] + 1;
-}
-else {
-    $newInterests = 0;
-}
+                        for (j = 0; j < interest_array.length; j++) {
+                            if (interest_array[j].selected == 1) {
+                                if (interest_count > 0) {
+                                    interest_list += "&";
+                                }
+                                interest_list += "interests[" + j + "]="
+                                    + interest_array[j].value;
+                                interest_count++;
+                            }
+                        }
 
-echo '<h3>' . helpTooltip('Add Author', 'addAuthorPageHelp') . '</h3>';
+                        temp_qs = temp_qs + interest_list;
+                    }
+                    else {
+                        temp_qs += form.elements[i].name + "="
+                            + form.elements[i].value;
+                    }
 
-$formAttr = array();
-if($_GET['popup'] != "false")
-    $formAttr = array('onsubmit' => "setTimeout('self.close()',0);");
+                    info_counter++;
+                }
+            }
 
-$form = new HTML_QuickForm('authorForm', 'post',
-                           "./add_publication.php?" . $_SERVER['QUERY_STRING'],
-                           "add_publication.php",
-                           $formAttr);
+            temp_qs.replace(" ", "%20");
+            temp_qs.replace("\"", "?");
+            location.replace("./add_author.php?{$_SERVER['QUERY_STRING']}&newInterests=" + num + "&" + temp_qs);
+        }
+        function closewindow() {window.close();}
 
-$form->addElement('text', 'firstname', null,
-                  array('size' => 50, 'maxlength' => 250));
-$form->addElement('text', 'lastname', null,
-                  array('size' => 50, 'maxlength' => 250));
-$form->addElement('text', 'auth_title', null,
-                  array('size' => 50, 'maxlength' => 250));
-$form->addElement('text', 'email', null,
-                  array('size' => 50, 'maxlength' => 250));
-$form->addElement('text', 'organization', null,
-                  array('size' => 50, 'maxlength' => 250));
-$form->addElement('text', 'webpage', null,
-                  array('size' => 50, 'maxlength' => 250));
+        function gotoAuthors() {
+            location.replace("../list_author.php?type=view&admin=true&newauthor=true");
+        }
 
-$interests = new pdAuthInterests();
-$interests->dbLoad($db);
-assert('is_array($interests->list)');
-foreach($interests->list as $intr) {
-    $options[$intr->interest_id] = $intr->interest;
-}
+        function resetAll() {
+            location.replace("./add_author.php?z{$_SERVER['QUERY_STRING']}&newInterests=0");
+        }
+        </script>
 
-$form->addElement('select', 'interests', null, $options,
-                      array('multiple' => 'multiple', 'size' => 4));
-
-$form->addElement('submit', 'Submit', 'Add Author',
-                  array('onClick' => 'return verify();'));
-$form->addElement('reset', 'Reset', 'Reset',
-                  array('class' => 'text', 'onClick' => 'resetAll();'));
-
-if ($_GET['popup'])
-    $form->addElement('reset', 'Cancel', 'Cancel',
-                      array('class' => 'text', 'onClick' => 'closeWindow();'));
-else
-    $form->addElement('reset', 'Cancel', 'Cancel',
-                      array('class' => 'text', 'onClick' => 'history.back();'));
-
-$form->addElement('hidden', 'newAuthorSubmitted', 'true');
-$form->addElement('hidden', 'numInterests', $counter + 1);
-$form->addElement('hidden', 'fromauthorspage', 'true');
-
-for ($i = 0; $i < $newInterests; $i++) {
-    $form->addElement('text', 'newInterest' . $i, null,
-                      array('size' => 50, 'maxlength' => 250));
-}
-
-$form->setDefaults($_GET);
-
-$renderer =& new HTML_QuickForm_Renderer_QuickHtml();
-$form->accept($renderer);
-
-
-$tableAttrs = array('width' => '100%',
-                    'border' => '0',
-                    'cellpadding' => '6',
-                    'cellspacing' => '0');
-$table = new HTML_Table($tableAttrs);
-$table->setAutoGrow(true);
-
-$table->addRow(array('First Name:', $renderer->elementToHtml('firstname')));
-$table->addRow(array('Last Name:', $renderer->elementToHtml('lastname')));
-$table->addRow(array(helpTooltip('Title', 'authTitleHelp') . ':',
-                     $renderer->elementToHtml('auth_title')));
-$table->addRow(array('Email:', $renderer->elementToHtml('email')));
-$table->addRow(array('Organization:',
-                     $renderer->elementToHtml('organization')));
-$table->addRow(array('Webpage:', $renderer->elementToHtml('webpage')));
-
-$ref = '<br/><div id="small"><a href="javascript:dataKeep(' . $newInterests
-    .')">[Add Interest]</a></div>';
-
-$table->addRow(array('Interests:' . $ref,
-                     $renderer->elementToHtml('interests')));
-
-for ($i = 0; $i < $newInterests; $i++) {
-$table->addRow(array('Interest Name:',
-                     $renderer->elementToHtml('newInterest' . $i)));
+JS_END;
+    }
 }
 
-echo $renderer->toHtml($table->toHtml()) . "</div>";
+$page = new add_author();
+echo $page->toHtml();
 
-$db->close();
-
-if ($_GET['popup'] != 'true') {
-    pageFooter();
-}
-
-echo '<script language="JavaScript" type="text/javascript" src="../wz_tooltip.js"></script>';
-
-echo "</body>\n</html>\n";
 
 ?>
