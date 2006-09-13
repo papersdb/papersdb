6bac33c7dcc55b1f77ab59467fe4e046a5ea527c
aicmltec <aicmltec>
2006-09-13 22:42:55 +0000

    fix for venue id selection when adding a publication

--- view_publication.php
+++ view_publication.php
@@ -1,6 +1,6 @@
 <?php ;
 
-// $Id: view_publication.php,v 1.56 2006/09/13 22:40:55 aicmltec Exp $
+// $Id: view_publication.php,v 1.57 2006/09/13 22:42:55 aicmltec Exp $
 
 /**
  * \file
@@ -22,7 +22,7 @@ require_once 'includes/pdAttachmentTypesList.php';
  * Renders the whole page.
  */
 class view_publication extends pdHtmlPage {
-    var $debug = 1;
+    var $debug = 0;
     var $pub_id;
 
     function view_publication() {
