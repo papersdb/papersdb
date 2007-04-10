0e0781a489f57f525e42e50eeb25108d80dda028
aicmltec <aicmltec>
2007-04-10 15:58:20 +0000

    missing file

---
+++ Admin/js/add_pub1.js
@@ -0,0 +1,45 @@
+<script language="JavaScript" type="text/JavaScript">
+
+ // $Id: add_pub1.js,v 1.1 2007/04/10 15:58:20 aicmltec Exp $
+
+var venueHelp=
+     "Where the paper was published -- specific journal, conference, "
+     + "workshop, etc. If many of the database papers are in the same "
+     + "venue, you can create a single <b>label</b> for that "
+     + "venue, to specify name of the venue, location, date, editors "
+     + "and other common information. You will then be able to use "
+     + "and re-use that information.";
+
+var categoryHelp=
+    "Category describes the type of document that you are submitting "
+    + "to the site. For examplethis could be a journal entry, a book "
+    + "chapter, etc.<br/><br/>"
+    + "Please use the drop down menu to select an appropriate "
+    + "category to classify your paper. If you cannot find an "
+    + "appropriate category you can select 'Add New Category' from "
+    + "the drop down menu and you will be asked for the new category "
+    + "information on a subsequent page.<br/><br/>";
+
+var titleHelp=
+    "Title should contain the title given to your document.";
+;
+
+var abstractHelp=
+    "Abstract is an area for you to provide an abstract of the "
+    + "document you are submitting.<br/><br/>"
+    + "To do this enter a plain text abstract for your paper in the "
+    + "field provided. HTML tags can be used.";
+
+
+var keywordsHelp=
+    "Keywords is a field where you can enter keywords that will be "
+    + "used to possibly locate your paper by others searching the "
+    + "database. You may want to enter multiple terms that are "
+    + "associated with your document. Examples may include words "
+    + "like: medical imaging; robotics; data mining.<br/><br/>"
+    + "Please enter keywords used to describe your paper, each "
+    + "keyword should be seperated by a semicolon.";
+
+var userInfoHelp
+= "A place for the user to enter his/her own information";
+</script>
