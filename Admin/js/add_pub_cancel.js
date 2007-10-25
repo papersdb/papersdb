0710b93460dd01b07c3d3b214903238b66122947
aicmltec <aicmltec>
2007-10-25 18:47:24 +0000

    fixed typo

--- Admin/js/add_pub_cancel.js
+++ Admin/js/add_pub_cancel.js
@@ -1,6 +1,6 @@
 function cancelConfirm() {
     var fRet;
-    result = confirm('Cancel all changes and stop adding / editing this publication antry?');
+    result = confirm('Cancel all changes and stop adding / editing this publication entry?');
     if (result) {
         window.location = 'http://{host}{new_location}';
     }
